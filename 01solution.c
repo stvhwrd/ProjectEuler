3c809bc636b71f2b8aca1db486989a0c25fe5726
Stevie Howard <stvhwrd@users.noreply.github.com>
2015-06-17 14:07:44 -0700

    Update 01solution.c

--- 01solution.c
+++ 01solution.c
@@ -3,14 +3,19 @@
 #include <math.h>
 #include <stdlib.h>
 
-int sumMultiples(int limit){
+int sumMultiples(int limit)
+{
     int sum = 0;
-    for(int i = 0; i < limit; i++){
+    
+    for(int i = 0; i < limit; i++)
+    {
     // using an if-else (instead of if-if) prevents the program from adding multiples of 15 twice (once for 3 and once for 5).
-        if(i%3==0){
+        if(i%3==0)
+        {
             sum+=i;
         }           
-        else if(i%5==0){
+        else if(i%5==0)
+        {
             sum+=i;
         }
     }
@@ -18,14 +23,16 @@ int sumMultiples(int limit){
 }
 
 
-int main() {
+int main() 
+{
     int testCases = 0, limit = 0;
     
-    // testCases simply consumes the first input (in this implementation)
+    // testCases consumes the first input (in this implementation)
     scanf("%i", &testCases);    
 
     //as long as we're reading in test cases
-    while(scanf("%d", &limit)==1){
+    while(scanf("%d", &limit) == 1)
+    {
     	// print the sum of 3 and/or 5 multiples less than the specified limit
         printf("%d\n", sumMultiples(limit));
     }
